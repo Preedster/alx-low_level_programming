635597993481ab0ca92f4090547c4c0fcca47611
Preedster <preciousjoseph453@gmail.com>
2023-09-10 02:34:16 -0700

    gcc -Wall -Werror -Wextra -pedantic -std=gnu89 4-puts.c && ./a.out

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,11 @@
 
 /**
  * main - this is the main put function
+ *
  * Return: 0
  */
 int main(void)
 {
-	puts("/"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
